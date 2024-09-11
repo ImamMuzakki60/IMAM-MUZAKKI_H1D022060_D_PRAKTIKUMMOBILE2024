ddd7330ddd19fecaf06493371d8e5705448e85c2
ImamMuzakki <imam.muzakki@mhs.unsoed.ac.id>
2024-09-11 15:31:49 +0700

    Initial commit

---
+++ Pertemuan1/dlm_article_autofill_v1/bin/dlm_article_autofill_v1.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+class ArtFill {
+  String? kegiatan;
+  String? definisi;
+  String? hariTanggal;
+  String? tempat;
+  String? tujuan;
+
+  ArtFill({this.kegiatan, this.hariTanggal, this.tempat, this.tujuan});
+
+  void setArtikel(){
+    print('DLM FT Unsoed — Telah berlangsung salah satu program kerja dari DLM FT Unsoed, yaitu $kegiatan pada $hariTanggal di $tempat. $kegiatan merupakan kegiatan $definisi. Kegiatan ini bertujuan untuk $tujuan.');
+  }
+ }
+
+void main() {
+  ArtFill artikel = ArtFill();
+  print('Masukkan nama kegiatan: ');
+  artikel.kegiatan = stdin.readLineSync();
+  print('Masukkan definisi kegiatan: ');
+  artikel.definisi = stdin.readLineSync();
+  print('Masukkan hari dan tanggal kegiatan (hari (tanggal/bulan/tahun)): ');
+  artikel.hariTanggal = stdin.readLineSync();
+  print('Masukkan tempat kegiatan: ');
+  artikel.tempat = stdin.readLineSync();
+  print('Masukkan tujuan kegiatan: ');
+  artikel.tujuan = stdin.readLineSync();
+  artikel.setArtikel();
+}
---
+++ Pertemuan1/dlm_article_autofill_v1/test/dlm_article_generator_v1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dlm_article_generator_v1/dlm_article_generator_v1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
